78b42b107bdb4f7cec23ec8ee271d48dfc1a7456
Nguyen Van Tho <iL3sor@github.com>
2022-11-14 19:53:44 +0700

    first commit

---
+++ webdev/menufud/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MenufudConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'menufud'
---
+++ webdev/textfud/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TextfudConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'textfud'

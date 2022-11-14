bf8eaa8c8766040aba85a2c084c714c2c43a0ff7
Nguyen Van Tho <iL3sor@github.com>
2022-11-14 20:59:00 +0700

    second commit

--- webdev/webdev/urls.py
+++ webdev/webdev/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('index/', include('home.urls')),
     path('admin/', admin.site.urls),
     path('textfud/', include('textfud.urls')),
     path('menufud/', include('menufud.urls')),

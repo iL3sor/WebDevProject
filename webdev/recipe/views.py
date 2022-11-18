6a5786bd362554db09aa00ef8f42be7fb2613119
iL3sor <92763293+iL3sor@users.noreply.github.com>
2022-11-19 02:15:39 +0700

    Update views.py

--- webdev/recipe/views.py
+++ webdev/recipe/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
 def index(request):
-    return HttpResponse("Recipe")
+    template = loader.get_template('recipe.html')
+    if(request.method=='POST' and request.POST['recipe']):
+        data = request.POST['recipe']
+        api_url = 'https://api.calorieninjas.com/v1/recipe?query='
+        query = data
+        response = requests.get(api_url + query, headers={'X-Api-Key': '3cYp06EjR3L15nU93CNiFA==nbMdfDFaPv8jImVt','Origin':'api.calorieninjas.com'})
+        res = response.json()
+        context = {
+        'title':res[0]['title'],
+        'servings':res[0]['servings'],
+        'ingredients':res[0]['ingredients'].split('|'),
+        'instructions':res[0]['instructions']
+        }
+        return HttpResponse(template.render(context))
+    else:
+        return HttpResponse(template.render())
